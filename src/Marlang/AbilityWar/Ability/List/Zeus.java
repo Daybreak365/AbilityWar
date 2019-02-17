d16bf7bbe51f9ba217877bbce8748777e9aea1bf
Marlang365 <39194432+Marlang365@users.noreply.github.com>
2019-02-17 15:48:18 +0900

    제우스 수정

--- src/Marlang/AbilityWar/Ability/List/Zeus.java
+++ src/Marlang/AbilityWar/Ability/List/Zeus.java
@@ -50,11 +50,11 @@ public class Zeus extends AbilityBase {
 		@Override
 		public void TimerProcess(Integer Seconds) {
 			if(Seconds.equals(3)) {
-				Circle = LocationUtil.getCircle(center, 3, 5, true);
+				Circle = LocationUtil.getCircle(center, 3, 7, true);
 			} else if(Seconds.equals(2)) {
-				Circle = LocationUtil.getCircle(center, 5, 5, true);
+				Circle = LocationUtil.getCircle(center, 5, 7, true);
 			} else if(Seconds.equals(1)) {
-				Circle = LocationUtil.getCircle(center, 7, 5, true);
+				Circle = LocationUtil.getCircle(center, 7, 7, true);
 			}
 			
 			for(Location l : Circle) {
